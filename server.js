1c002097bece94d1248cc2429d927b20c8b1404e
Ahmad Raza Soft.Engr <ahmad.raza916@gmail.com>
2018-12-29 19:38:53 +0500

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -30,4 +31,7 @@ app.get('/bad',(req,res) => {
         
     });
     });
-app.listen(3000);
+
+app.listen(port, () => {
+    console.log(`load data from port ${port}`);
+});
